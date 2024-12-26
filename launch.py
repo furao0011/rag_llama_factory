516555272484b491dc2352bbbac38ee08844609d
luyuan <furao0011@gmail.com>
2024-12-26 20:25:36 +0800

    feat:extend web with RAG

---
+++ launch.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+from subprocess import run
+run(["llamafactory-cli", "webchat", "--model_name_or_path", "models/Qwen2-0.5B-Medical-MLX", "--template", "qwen"])
+# run(["llamafactory-cli", "webui"])
