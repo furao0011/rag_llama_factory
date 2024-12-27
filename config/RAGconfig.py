61cf1efaab529795ab0bc055608fb49d939dbe21
luyuan <furao0011@gmail.com>
2024-12-27 12:59:39 +0800

    fix:修改方法注释提供模板

--- config/RAGconfig.py
+++ config/RAGconfig.py
@@ -1,6 +1,22 @@
+"""
+if there is an interface, example:
+from multiprocessing.connection import Client
 
-# define Your RAG method here
-# if u use an interface, plz use flask or others method to RAG
+def RAG(user_query: str) -> str:
+    try:
+        # 使用 with 语句来管理连接
+        with Client(('localhost', 6000)) as conn:
+            # 发送用户查询
+            conn.send(user_query)
+
+            # 接收 RAG 处理后的结果
+            result = conn.recv()
+
+        return result
+
+    except Exception as e:
+        return f"Error: {e}"
+"""
 def RAG(user_query: str) -> str:
     processed_query = "我脖子疼," + user_query
     return processed_query
--- launch.py
+++ launch.py
@@ -3,5 +3,5 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 from subprocess import run
-run(["llamafactory-cli", "webchat", "--model_name_or_path", "models/Qwen2-0.5B-Medical-MLX", "--template", "qwen"])
-# run(["llamafactory-cli", "webui"])
+# run(["llamafactory-cli", "webchat", "--model_name_or_path", "models/Qwen2-0.5B-Medical-MLX", "--template", "qwen"])
+run(["llamafactory-cli", "webui"])
